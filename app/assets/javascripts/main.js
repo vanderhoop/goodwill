dab71104abb8b6f294ce7fa030ddc98bcffde696
vanderhoop <vanderhoop@me.com>
2014-06-01 12:10:00 -0400

    textarea now completely aligned with <pre>formatted text

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,10 +6,11 @@ $(document).ready(function(){
       $userInput = $("#user-text").text();
       $generatedAppendage = $("#generated-text").text();
       filler = {
-        139: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus e",
-        138: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus ",
-        137: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus",
-        136: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibu",
+        140: "",
+        139: " | You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for ",
+        138: " | murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that se",
+        137: " | ven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the sno",
+        136: " | w that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.",
         135: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatib",
         134: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penati",
         133: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penat",
@@ -19,6 +20,11 @@ $(document).ready(function(){
         119: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque p",
         118: " | Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque "
       };
+
+
+  $("#user-text").on('onkeydown', function(){
+    console.log("proven");
+  });
 
   $formField.on("input", function(e){
     console.log($(this).val());
@@ -40,6 +46,6 @@ $(document).ready(function(){
 
   function appendText(){
     var stringToAppend = filler[determineAppropriateLength()];
-    $("#generated-text").text(stringToAppend || " | fartstore");
+    $("#generated-text").text(stringToAppend || " | fartstore").fadeIn();
   };
 }); //document.ready
